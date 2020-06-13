1fa3b312d243a218b204bf5fba3d6bc79af8a708
Travis Hinkelman <travis.hinkelman@gmail.com>
2020-06-13 15:11:06 -0700

    Change get_water_year_type to return NA if water year is not present

--- R/get_water_year_type.R
+++ R/get_water_year_type.R
@@ -8,9 +8,12 @@
 #'
 #' @export
 #' @examples
-#' get_water_year_type(1997:2011)
+#' get_water_year_type(1900:1906)
+#' get_water_year_type(1900:1906, "SJR")
+#' get_water_year_type(2014:2020)
 
 get_water_year_type <- function(water_year, valley = c("SAC", "SJR")){
   valley <- match.arg(valley)
-  water_year_type$WaterYearType[water_year_type$WaterYear %in% water_year & water_year_type$Valley == valley]
+  tmp <- water_year_type[water_year_type$Valley == valley, ]
+  tmp$WaterYearType[match(water_year, tmp$WaterYear)]
 }
