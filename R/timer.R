89654eb7d8ee0648441e9c6b66f4cccce3ca7875
Myfanwy <myfanwy@github.com>
2021-01-26 00:13:14 -0800

    fix timer doc

--- R/timer.R
+++ R/timer.R
@@ -3,6 +3,9 @@
 #' @md
 #' @param interval The length of time you want
 #' @param units The unit of time you want to use, i.e. "secs", "mins", "hours"
+#' @param sound Your sound of choice from beepr::beep
+#' @return a sound from the beepr pkg; options are 1-11.  Defaults to 2
+#' @importFrom beepr beep
 #' @export
 #' @examples
 #' timer(45, "mins")
