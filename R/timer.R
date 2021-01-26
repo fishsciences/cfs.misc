e3048d21e8e498ab9db895ae22197d2f1d639561
Myfanwy <myfanwy@github.com>
2021-01-25 23:53:43 -0800

    fix bad beepr dependency, rm .rproj

--- R/timer.R
+++ R/timer.R
@@ -3,19 +3,19 @@
 #' @md
 #' @param interval The length of time you want
 #' @param units The unit of time you want to use, i.e. "secs", "mins", "hours"
-#'
+#' @importFrom beepr beep
 #' @export
 #' @examples
 #' timer(45, "mins")
 #'
 
-timer <- function(interval, units) {
-  require(beepr)
+timer <- function(interval = 1, units = "secs", beep = 2) {
+
   t0 <- Sys.time()
   stopwatch <- round(as.double(difftime(Sys.time(), t0, u = units)))
 
   while(stopwatch < interval){
     stopwatch <- round(as.double(difftime(Sys.time(), t0, u = units)))
   }
-  beep(2)
+  beepr::beep(beep)
 }
